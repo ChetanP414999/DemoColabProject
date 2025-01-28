3b9c5ab186c5f0046c97d0e35d62876fd624d374
cheta <chetanpatil2521993@gmail.com>
2025-01-28 15:20:02 +0530

    Student consume added...

---
+++ DemoColabProject-Student-Consume/src/main/java/com/cjc/controller/StudentController.java
@@ -0,0 +1,11 @@
+package com.cjc.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+	
+	
+	
+}
---
+++ DemoColabProject-Student-Consume/src/main/java/com/cjc/serviceimpl/StudentServiceImpl.java
@@ -0,0 +1,10 @@
+package com.cjc.serviceimpl;
+
+import org.springframework.stereotype.Service;
+
+import com.cjc.service.StudentService;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+}
